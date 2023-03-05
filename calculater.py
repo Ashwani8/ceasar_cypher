ab8eb06d0441674b026754b8892337a4402aa6ab
Ashwani Kumar <ashwani.oct8@gmail.com>
2023-03-05 14:29:42 -0600

    introduced mathematical capabilities

--- calculater.py
+++ calculater.py
@@ -18,11 +18,21 @@ def divide(n1, n2):
 
 # create a dictionary with math symbole as key and corresponsing operations as values
 math_operation = {
+    # This is clever, we are not using operation(n1,n2), but just a string, (num1,num2) is concocnated later
     "+" : add,
     "-" : subtract,
     "*" : multiply,
     "/" : divide,
 
 }
-num1 = int(input("Enter first number: "))
-num1 = int(input("Enter second number: "))
+num1 = int(input("Enter first number?: "))
+
+for symbol in math_operation:
+    print(symbol)
+
+operation_symbol = input("Pick an operation from the line above: ")
+num2 = int(input("Enter second number?: "))
+calculation_function = math_operation[operation_symbol]
+# This concatnate simple text (add/subtract) with (n1,n2) in the line below
+answer = calculation_function(num1, num2) 
+print(f"{num1} {operation_symbol} {num2} = {answer}")
