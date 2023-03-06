ec7af4af22d1eaf3d1cdfd87717512f84e3839d3
Ashwani Kumar <ashwani.oct8@gmail.com>
2023-03-06 12:38:59 -0600

    score calculation and check for blackjack

--- BlackJack.py
+++ BlackJack.py
@@ -53,16 +53,21 @@ print(user_cards)
 #and returns the score. 
 #Look up the sum() function to help you do this.
 def calculate_score(card_list):
+    """Calculate score from the cards"""
+#Hint 7: Inside calculate_score() check for a blackjack (a hand with only 2 cards: ace + 10) and return 0 instead of the actual score. 0 will represent a blackjack in our game.
+    # if 11 in cards and 10 in cards and len(cards) ==2:
+    if sum(cards) == 21 and len(cards) == 2:
+        return 0
+#Hint 8: Inside calculate_score() check for an 11 (ace). If the score is already over 21, remove the 11 and replace it with a 1. You might need to look up append() and remove().
+    if 11 in cards and sum(cards) > 21 :
+        cards.remove(11)
+        cards.append(1)
+#Hint 9: Call calculate_score(). If the computer or the user has a blackjack (0) or if the user's score is over 21, then the game ends.
     return sum(card_list)
+
 user_score = calculate_score(user_cards)
 computer_score = calculate_score(computer_cards)
 print(user_score)
-#Hint 7: Inside calculate_score() check for a blackjack (a hand with only 2 cards: ace + 10) and return 0 instead of the actual score. 0 will represent a blackjack in our game.
-
-#Hint 8: Inside calculate_score() check for an 11 (ace). If the score is already over 21, remove the 11 and replace it with a 1. You might need to look up append() and remove().
-
-#Hint 9: Call calculate_score(). If the computer or the user has a blackjack (0) or if the user's score is over 21, then the game ends.
-
 #Hint 10: If the game has not ended, ask the user if they want to draw another card. If yes, then use the deal_card() function to add another card to the user_cards List. If no, then the game has ended.
 
 #Hint 11: The score will need to be rechecked with every new card drawn and the checks in Hint 9 need to be repeated until the game ends.
