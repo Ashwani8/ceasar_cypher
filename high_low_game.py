d99956c46a9dea07fe640bad687eefa96a9e8d78
Ashwani Kumar <ashwani.oct8@gmail.com>
2023-03-07 19:19:01 -0600

    format data and display

--- high_low_game.py
+++ high_low_game.py
@@ -4,40 +4,25 @@ from art import vs
 import high_low_game_data as data # not a good choice as i have to use data.data
 
 from high_low_game_data import data
+score = 0
+def format_data(account):
+   """Format the account data into printable format"""
+   account_name = account['name']
+   account_discr = account['description']
+   account_country = account['country']
+   account_summary = f" {account_name}, {account_discr}, {account_country} "
+   return account_summary
+print(high_low_logo)
 account_a = random.choice(data)
 account_b = random.choice(data)
 if account_a == account_b:
     account_b = random.choice(data)
- 
-# data_list = [{
-#         'name': 'Instagram',
-#         'follower_count': 346,
-#         'description': 'Social media platform',
-#         'country': 'United States'
-#     },
-#     {
-#         'name': 'Cristiano Ronaldo',
-#         'follower_count': 215,
-#         'description': 'Footballer',
-#         'country': 'Portugal'
-#     },
-#     {
-#         'name': 'Ariana Grande',
-#         'follower_count': 183,
-#         'description': 'Musician and actress',
-#         'country': 'United States'
-#     },
-#     {
-#         'name': 'Dwayne Johnson',
-#         'follower_count': 181,
-#         'description': 'Actor and professional wrestler',
-#         'country': 'United States'
-#     },]
-# 
-print(high_low_logo)
-print(f"Compare A, {account_a}")
+a_count =account_a['follower_count']
+b_count = account_b['follower_count']
+
+print(f"Compare A, {format_data(account_a)}")
 # display score only after first attempt
-# initial_score = 0
+
 # define a function to randomly select from data 
 # for item in data:
 #     print (item)
@@ -60,7 +45,7 @@ print(f"Compare A, {account_a}")
 # any other answer is treated as incorrect
 
 print(vs)
-print(f"Compare B, {account_b}")
+print(f"Compare B, {format_data(account_b)}")
 # Display option B
 # print(f"Compare B,{a_followers} ")
 # user_choice = input("Who has more followers? Type 'A' or 'B': ").lower()
@@ -69,8 +54,8 @@ print(f"Compare B, {account_b}")
 # # and replace choice A with B, and fetch a new data account for choice a , game continue
 # a_option = b_option
 # b_option = new_account()
-# current_score = 0
-# print(f" You're right! Current score {current_score}")
+
+print(f" You're right! Current score {score}")
 # # if incorrect, print score, and game ends
 
-# print(f"Sorry, that's wrong, Final score {current_score}")
+print(f"Sorry, that's wrong, Final score {score}")
